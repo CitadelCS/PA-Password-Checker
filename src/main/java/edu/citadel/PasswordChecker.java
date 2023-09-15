2b0fa97cd1782eba4e5422e140ba929b004800ea
mverdicchio <verdicchio@gmail.com>
2023-09-15 17:01:31 -0400

    starter code and tests ready

--- src/main/java/edu/citadel/PasswordChecker.java
+++ src/main/java/edu/citadel/PasswordChecker.java
@@ -8,6 +8,26 @@ public class PasswordChecker {
         // write your program here.
         // Don't forget to plan and write pseudocode.
         // Post to discord if you have questions.
+        Scanner input = new Scanner(System.in);
+        System.out.println("ENTER PASSWORD PLEASE:");
+        String pw = input.nextLine();
+
+        // you can make any variables you think will help:
+
+
+
+    
+        // this loop will iterate over each character in the string
+        // ch will be the current character
+        // the loop will end when all characters have been processed
+        for (char ch : pw.toCharArray()) {
+            // add code in here to check the current character
+
+
+        }
+    
+        //end the program by printing GOOD or BAD depending on what you saw.
+    
 
     }
 }
--- src/test/java/edu/citadel/PasswordCheckerTest.java
+++ src/test/java/edu/citadel/PasswordCheckerTest.java
@@ -15,109 +15,119 @@ import org.junit.Test;
  * Unit test for simple App.
  */
 public class PasswordCheckerTest {
-    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-    private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+  private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+  private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
 
-    @Before
-    public void setUpStreams() {
-        System.setOut(new PrintStream(outContent));
-        System.setErr(new PrintStream(errContent));
-    }
+  @Before
+  public void setUpStreams() {
+    System.setOut(new PrintStream(outContent));
+    System.setErr(new PrintStream(errContent));
+  }
 
-    @After
-    public void restoreStreams() {
-        System.setOut(System.out);
-        System.setErr(System.err);
-    }
+  @After
+  public void restoreStreams() {
+    System.setOut(System.out);
+    System.setErr(System.err);
+  }
 
+  @Test
+  public void testPassLength1() {
+    String consoleInput = "WAYTOOLONG";
+    InputStream in = new ByteArrayInputStream(consoleInput.getBytes());
+    System.setIn(in);
+    PasswordChecker.main(null);
+    String result = outContent.toString();
+    assertTrue("WAYTOOLONG - bad", result.toLowerCase().contains("bad"));
+  }
 
-    public void testPass1() {
-        setSystemIn("WAYTOOLONG");
-        PasswordChecker.main(null);
-        assertTrue("WAYTOOLONG - bad", systemOut().getHistory().toLowerCase().contains("bad"));
-      }
-    
-      public void testPass2() {
-        setSystemIn("short");
-        PasswordChecker.main(null);
-        assertTrue("short - bad", systemOut().getHistory().toLowerCase().contains("bad"));
-      }
-    
-      public void testPass3() {
-        setSystemIn("lcase12*");
-        PasswordChecker.main(null);
-        assertTrue("lcase12* - bad", systemOut().getHistory().toLowerCase().contains("bad"));
-      }
-    
-      public void testPass4() {
-        setSystemIn("UCASE12*");
-        PasswordChecker.main(null);
-        assertTrue("UCASE12* - bad", systemOut().getHistory().toLowerCase().contains("bad"));
-      }
-    
-      public void testPass5() {
-        setSystemIn("N0Symbls");
-        PasswordChecker.main(null);
-        assertTrue("N0Symbls - bad", systemOut().getHistory().toLowerCase().contains("bad"));
-      }
-    
-      public void testPass6() {
-        setSystemIn("b4dSMBL%");
-        PasswordChecker.main(null);
-        assertTrue("b4dSMBL% - bad", systemOut().getHistory().toLowerCase().contains("bad"));
-      }
-    
-      public void testPass7() {
-        setSystemIn("g00dPAS!");
-        PasswordChecker.main(null);
-        assertTrue("g00dPAS! - good", systemOut().getHistory().toLowerCase().contains("good"));
-      }
-    
-      public void testPass8() {
-        setSystemIn("g00dPAS?");
-        PasswordChecker.main(null);
-        assertTrue("g00dPAS? - good", systemOut().getHistory().toLowerCase().contains("good"));
-      }
-    
-      public void testPass9() {
-        setSystemIn("g00dPAS*");
-        PasswordChecker.main(null);
-        assertTrue("g00dPAS* - good", systemOut().getHistory().toLowerCase().contains("good"));
-      }
-    
-      public void testPass10() {
-        setSystemIn("?d0CV33!");
-        PasswordChecker.main(null);
-        assertTrue("?d0CV33! - good", systemOut().getHistory().toLowerCase().contains("good"));
-      }
-/*   
-    @Test
-    public void testYouRolled() {
-        String consoleInput = "5";
-        InputStream in = new ByteArrayInputStream(consoleInput.getBytes());
-        System.setIn(in);
-        PasswordChecker.main(null);
-        String result = outContent.toString();
-        assertTrue("prints \"you rolled\" message", result.toLowerCase().contains("you rolled"));
-    }
+  @Test
+  public void testPassLength2() {
+    String consoleInput = "short";
+    InputStream in = new ByteArrayInputStream(consoleInput.getBytes());
+    System.setIn(in);
+    PasswordChecker.main(null);
+    String result = outContent.toString();
+    assertTrue("short - bad", result.toLowerCase().contains("bad"));
+  }
 
-    @Test
-    public void testWinnerStatements() {
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < 500; i++) {
-            String consoleInput = "5";
-            InputStream in = new ByteArrayInputStream(consoleInput.getBytes());
-            System.setIn(in);
-            PasswordChecker.main(null);
-            String result = outContent.toString();
-            sb.append(result.toLowerCase());
-        }
-        String s = sb.toString();
-        assertTrue("big winner possible after many rolls", s.contains("big winner"));
-        assertTrue("medium winner possible after many rolls", s.contains("medium winner"));
-        assertTrue("small winner possible after many rolls", s.contains("small winner"));
-        assertTrue("draw possible after many rolls", s.contains("draw"));
-        assertTrue("you lose possible after many rolls", s.contains("you lose"));
-    }
-*/
+  @Test
+  public void testPassCase1() {
+    String consoleInput = "lcase12*";
+    InputStream in = new ByteArrayInputStream(consoleInput.getBytes());
+    System.setIn(in);
+    PasswordChecker.main(null);
+    String result = outContent.toString();
+    assertTrue("lcase12* - bad", result.toLowerCase().contains("bad"));
+  }
+
+  @Test
+  public void testPassCase2() {
+    String consoleInput = "UCASE12*";
+    InputStream in = new ByteArrayInputStream(consoleInput.getBytes());
+    System.setIn(in);
+    PasswordChecker.main(null);
+    String result = outContent.toString();
+    assertTrue("UCASE12* - bad", result.toLowerCase().contains("bad"));
+  }
+
+  @Test
+  public void testPassSymbol1() {
+    String consoleInput = "N0Symbls";
+    InputStream in = new ByteArrayInputStream(consoleInput.getBytes());
+    System.setIn(in);
+    PasswordChecker.main(null);
+    String result = outContent.toString();
+    assertTrue("N0Symbls - bad", result.toLowerCase().contains("bad"));
+  }
+
+  @Test
+  public void testPassSymbol2() {
+    String consoleInput = "b4dSMBL%";
+    InputStream in = new ByteArrayInputStream(consoleInput.getBytes());
+    System.setIn(in);
+    PasswordChecker.main(null);
+    String result = outContent.toString();
+    assertTrue("b4dSMBL% - bad", result.toLowerCase().contains("bad"));
+  }
+
+  @Test
+  public void testPassGood1() {
+    String consoleInput = "g00dPAS!";
+    InputStream in = new ByteArrayInputStream(consoleInput.getBytes());
+    System.setIn(in);
+    PasswordChecker.main(null);
+    String result = outContent.toString();
+    assertTrue("g00dPAS! - good", result.toLowerCase().contains("good"));
+  }
+
+  @Test
+  public void testPassGood11() {
+    String consoleInput = "g00dPAS?";
+    InputStream in = new ByteArrayInputStream(consoleInput.getBytes());
+    System.setIn(in);
+    PasswordChecker.main(null);
+    String result = outContent.toString();
+    assertTrue("g00dPAS? - good", result.toLowerCase().contains("good"));
+  }
+
+  @Test
+  public void testPassGood2() {
+    String consoleInput = "g00dPAS*";
+    InputStream in = new ByteArrayInputStream(consoleInput.getBytes());
+    System.setIn(in);
+    PasswordChecker.main(null);
+    String result = outContent.toString();
+    assertTrue("g00dPAS* - good", result.toLowerCase().contains("good"));
+  }
+
+  @Test
+  public void testPassGood22() {
+    String consoleInput = "?d0CV33!";
+    InputStream in = new ByteArrayInputStream(consoleInput.getBytes());
+    System.setIn(in);
+    PasswordChecker.main(null);
+    String result = outContent.toString();
+    assertTrue("?d0CV33! - good", result.toLowerCase().contains("good"));
+  }
+
 }
